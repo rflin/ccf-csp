4b6c2f7e5bb5a9e2fed0d648d454ae5ae22a1978
L <462650965@qq.com>
2018-02-27 15:25:13 +0800

    -

--- 201712-2 游戏 ccf.cpp
+++ 201712-2 游戏 ccf.cpp
@@ -1,28 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> getNext(vector<int> &v,int k,int &num)
-{
-    vector<int> nextv;
-    int n=v.size(),i=0;
-    while(i<n)
-    {
-        if((num+i+1)%10==k||(num+i+1)%k==0) ++i;
-        else nextv.push_back(v[i++]);
-    }
-    num+=v.size();
-    if(nextv.empty())
-        nextv.push_back(v.back());
-    return nextv;
-}
 int main()
 {
-    int n,k;
-    vector<int> v;
-    scanf("%d %d",&n,&k);
-    for(int i=1;i<=n;++i)
-        v.push_back(i);
-    int num=0;
-    while(v.size()>1) v=getNext(v,k,num);
-    cout<<v[0]<<endl;
-    return 0;
+	vector<int> v;
+	int n,k;
+	cin>>n>>k;
+	for(int i=1;i<=n;++i)
+		v.push_back(i);
+	int num=0;
+	while(v.size()>1)
+	{
+		n=v.size();
+		int len=0;
+		for(int i=0;i<n;++i)
+		{
+			++num;
+			if(num%k&&num%10!=k) v[len++]=v[i];
+		}
+		if(len==0) v[len++]=v.back();
+		v.resize(len);
+	}
+	cout<<v[0]<<endl;
+	return 0;
 }
